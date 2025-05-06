eb3baadf1d8b702f61943cfd64774d8033e16ce3
Sameul-ziger <srziger3@gmail.com>
2025-05-05 22:31:46 -0300

    ajuste de rota pra vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          mui: ['@mui/material', '@mui/icons-material']
+        }
+      }
+    }
+  },
+  base: '/'
 })
