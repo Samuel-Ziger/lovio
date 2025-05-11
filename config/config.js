9e1f63d76d50ee800a3a1291c4125adeb654105b
Sameul-ziger <srziger3@gmail.com>
2025-05-11 17:07:34 -0300

    Alteração de dados pessoais

--- config/config.js
+++ config/config.js
@@ -5,6 +5,9 @@ module.exports = {
   frontendUrl: process.env.FRONTEND_URL || 'http://localhost:5173',
   backendUrl: process.env.BACKEND_URL || 'http://localhost:5001',
   mercadoPago: {
-    accessToken: 'APP_USR-4868459967001491-051003-be2cae39860e8eb714f547165324245f-305462343'
+    accessToken: 'APP_USR-4868459967001491-051003-be2cae39860e8eb714f547165324245f-305462343',
+    publicKey: 'APP_USR-fc533834-0b64-46f1-88ff-f4a610774e2d',
+    clientId: process.env.MERCADO_PAGO_CLIENT_ID,
+    clientSecret: process.env.MERCADO_PAGO_CLIENT_SECRET
   }
 }; 
