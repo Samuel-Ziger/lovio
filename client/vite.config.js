ad269d2b331380c6b1c202fff625decb364a404e
Sameul-ziger <srziger3@gmail.com>
2025-05-21 20:45:13 -0300

    nao aguento mais

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,7 +22,13 @@ export default defineConfig({
       }
     }
   },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
   server: {
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
